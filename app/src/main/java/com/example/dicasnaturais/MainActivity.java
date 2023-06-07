ed0f79a6d751b008578ffa5f7574f464d737374c
Guilherme <guilherme_fcm@hotmail.com>
2023-06-07 20:06:20 -0300

    Improve navigation

--- app/src/main/java/com/example/dicasnaturais/MainActivity.java
+++ app/src/main/java/com/example/dicasnaturais/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.profile_menu, menu);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         MenuItem searchItem = menu.findItem(R.id.search);
         SearchView searchView = (SearchView) searchItem.getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -79,16 +81,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.profile) {
+
+        if (id == R.id.profile)
             render( new ProfileFragment(db.tipDao()) );
-            return true;
-        } else if (id == R.id.tips) {
+
+        else if (id == R.id.tips)
             Toast.makeText(this, "Minhas dicas", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (id == R.id.settings) {
+
+        else if (id == R.id.settings)
             Toast.makeText(this, "Configurações", Toast.LENGTH_SHORT).show();
-            return true;
-        } else return super.onOptionsItemSelected(item);
+
+        else if (id == android.R.id.home)
+            render( new ListTipsFragment(db.tipDao()) );
+
+        return true;
     }
 
     private void render(Fragment fragment) {
